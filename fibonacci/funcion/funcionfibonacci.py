d6218e7d3de8fc619e581f1ab50c5e355d486ea8
paulara <paulara99@gmail.com>
2016-11-23 11:30:44 +0100

    version1fibonacci

---
+++ fibonacci/funcion/funcionfibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci (n):
+    if n==1:
+        return 1
+    if n==2:
+        return 1
+    else:
+        return (n-1)+fibonacci(n-2)
+if __name__=="__main__":
+    n=int(input("Introduzca un numero"))
+    fibonacci(n)
+    print(fibonacci(n))
